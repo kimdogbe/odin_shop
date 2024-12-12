1d4903c92e1c8e0a72336a8c5e612e9bb81012cb
kimdogbe <kimdogbe@gmail.com>
2024-12-12 18:30:37 +0000

    Implement ProductCard compoenent with CSS

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
+import ProductCard from "./components/ProductCard"
 
 function Shop() {
   return (
     <div>
       <h1>Shop page</h1>
       <p>Browse from our products below!</p>
+      <ProductCard />
     </div>
   )
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,16 @@
-import AddBtn from "./AddBtn"
+import Button from "./AddBtn"
+import stool from "../assets/stool.jpg"
 
-function ProductCard() {
-  return <div>
-    <p>Product card</p>
-    <AddBtn />
+function ProductCard({ name="High Stool", description="Sweedish designed high stool", image=stool, price="£1" }) {
+  return <div className="product">
+    <img src={image} alt={description} />
+    <div>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>{price}</p>
+    </div>
+
+    <Button text="Add to Cart"/>
   </div>
 }
 
