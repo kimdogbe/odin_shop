3e8afcfb317fe2879a005eef630831bb098aa013
kimdogbe <kimdogbe@gmail.com>
2024-12-16 10:01:54 +0000

    Improve styling for checkout page

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,26 +1,29 @@
-import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
+import { useOutletContext } from "react-router-dom";
 
-function Checkout({ cart }) {
+function Checkout() {
+  const [cart, setCart] = useOutletContext();
+
+  console.log("Checkout:")
+  console.log(cart);
+  
+
   return (
-    <div>
+    <div className="checkout">
       <h1>Checkout</h1>
       <p>Hello from the checkout page where we'll put content we have selected to buy.</p>
-      {cart.map((item) => {
-        <div>
-          <img src={item.image} alt={item.name} />
-          <p>{item.name}</p>
-          <p>£{item.price}</p>
-          <p>{item.count}</p>
-          <p>Total: £{item.price * item.count}</p>
-        </div>
-      })}
+      {
+        cart.map(item => 
+          <div className="cart-item" key={item.id}>
+            <img src={item.image} alt={item.title} />
+            <p>{item.title}</p>
+            <p>£{item.price} x {item.count}</p>
+            <p>Total: £{(item.price * item.count).toFixed(2)}</p>
+          </div>
+        )
+      }
     </div>
   )
-}
-
-Checkout.propTypes = {
-  cart: PropTypes.object
 }
 
 export default Checkout
